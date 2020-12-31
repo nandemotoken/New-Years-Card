7c6c48bf53726c365508e72032f35f7c10629819
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2020-12-31 12:59:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello_world():
       f.writelines( request.args.get('address') + "\n" )
     except:
       return 'アドレスを取得できません'
-  return '年賀状をお待ちください'
+  return 'NFT年賀状の配布ページは1月1日公開です。<a href="https://twitter.com/NandemoToken">Twitter</a>でご案内します。お待ちください。</br><img src="https://raw.githubusercontent.com/nandemotoken/New-Years-Card-Server/gh-pages/hagaki.png">'
